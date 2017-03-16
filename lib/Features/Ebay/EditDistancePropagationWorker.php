0654cd9ed3f1e9965e1bb19717fd9a5126517dd7
Fabrizio Regini <freegenie@gmail.com>
2017-03-16 10:43:07 +0100

    add missing method in EditDistancePropagationWorker

--- lib/Features/Ebay/EditDistancePropagationWorker.php
+++ lib/Features/Ebay/EditDistancePropagationWorker.php
@@ -11,7 +11,8 @@ namespace Features\Ebay ;
 
 use \TaskRunner\Commons\AbstractWorker;
 use TaskRunner\Commons\AbstractElement;
-use TaskRunner\Commons\QueueElement ;
+use TaskRunner\Commons\QueueElement;
+use TaskRunner\Exceptions\EndQueueException;
 
 class EditDistancePropagationWorker extends AbstractWorker {
 
@@ -30,6 +31,22 @@ class EditDistancePropagationWorker extends AbstractWorker {
                 $queueElement->params
         );
 
+    }
+
+    public function _checkForReQueueEnd( QueueElement $queueElement ) {
+        /**
+         * check for loop re-queuing
+         */
+        if ( isset( $queueElement->reQueueNum ) && $queueElement->reQueueNum >= 100 ) {
+
+            $msg = "\n\n Error Set Contribution  \n\n " . var_export( $queueElement, true );
+            \Utils::sendErrMailReport( $msg );
+            $this->_doLog( "--- (Worker " . $this->_workerPid . ") :  Frame Re-queue max value reached, acknowledge and skip." );
+            throw new EndQueueException( "--- (Worker " . $this->_workerPid . ") :  Frame Re-queue max value reached, acknowledge and skip.", self::ERR_REQUEUE_END );
+
+        } elseif ( isset( $queueElement->reQueueNum ) ) {
+            $this->_doLog( "--- (Worker " . $this->_workerPid . ") :  Frame re-queued {$queueElement->reQueueNum} times." );
+        }
     }
 
 }
