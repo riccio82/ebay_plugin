248f2bbd24ff24b04c12000b0fd64c790f7005f6
riccio82 <riccio82@gmail.com>
2017-05-02 16:40:51 +0200

    Fix to the analyze page

---
+++ static/src/js/ebay-analyze.js
@@ -0,0 +1,12 @@
+
+$.extend( UI, {
+
+    setTranslateButtonEvent: function () {},
+    pollData: function () {
+        $( '.mergebtn, .splitbtn' ).removeClass( 'disabled' )
+    },
+    setFocusEvent: function () {},
+    getProjectInfo: function () {}
+
+});
+
