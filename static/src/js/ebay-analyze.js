702cdc1afb2f8821fefe72480992abf68a971ffa
Fabrizio Regini <freegenie@gmail.com>
2017-06-15 09:30:41 +0200

    fix ebay analyze page after changes to pollData

--- static/src/js/ebay-analyze.js
+++ static/src/js/ebay-analyze.js
@@ -2,11 +2,11 @@
 $.extend( UI, {
 
     setTranslateButtonEvent: function () {},
-    pollData: function () {
-        $( '.mergebtn, .splitbtn' ).removeClass( 'disabled' )
-    },
     setFocusEvent: function () {},
     getProjectInfo: function () {}
 
 });
 
+$(function() {
+    $( '.mergebtn, .splitbtn' ).removeClass( 'disabled' ) ;
+});
